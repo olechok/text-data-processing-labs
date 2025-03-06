045eb88c53642260de9a4dc11989cb22789c085c
OlhaMish <olgami1402@gmail.com>
2025-03-06 21:23:01 +0200

    feat: add task1: card encoder

---
+++ lab1/task2/card_encoder.py
@@ -0,0 +1,20 @@
+import re
+from utils.get_text_from_file import read_text_from_file
+
+
+# CARD_PATTERN = r'\b(\d{2})([\d\s-]{14})\b'
+# CARD_PATTERN2 = r'\b(\d{2})\d{2} \d{4} \d{4} \d{4}\b'
+CARD_PATTERN = r'\b(\d{2})(\d{2})[\s-]?(\d{4})[\s-]?(\d{4})[\s-]?(\d{4})\b'
+
+def mask_bank_cards(text: str) -> str:
+    text = re.sub(CARD_PATTERN, r'\1????????????????', text)
+
+    return text
+
+
+if __name__ == '__main__':
+    text = read_text_from_file("../text4.txt")
+    print(text)
+    masked_text = mask_bank_cards(text)
+    print(masked_text)
+
